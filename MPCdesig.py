4170946f20a56e6b73e2b2b777dc32976e86baa2
DrTapiT <trinitymt7@gmail.com>
2022-03-13 15:37:48 +0100

    apply strip directly on string instead of calling to_str

--- MPCdesig.py
+++ MPCdesig.py
@@ -305,7 +305,7 @@ def check_valid_prov_desig(input_desig):
     """
 
     try:
-        input_str = to_str(input_desig)
+        input_str = str(input_desig).strip()
         return check_packed_unpacked(input_str, reProv, rePackedProv)
     except:
         return False
